ddb2c00645408d1b8208317f680aff24a65450f9
Student <student@it.uekat.local>
2024-01-14 16:32:09 +0100

    Technologie chmurowe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import urllib.error
+import urllib.request
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -22,7 +24,31 @@ class PeopleCounter(Resource):
         return {'peopleCount': len(rects)}
 
 
+class PeopleCounterDynamicUrl(Resource):
+    def get(self):
+             # here we want to get the value of user (i.e. ?user=some-value)
+        user = request.args.get('user')
+        # 1. Pobrać zdjęcie z otrzymanego adresu
+        # 2. Pobrane zdjęcie można zapisać na dysku lub przetwarzać je w pamięci podręcznej
+        # 3. Załadowane zjęcie do zmiennej image przekazać do algorytmu hog.detectMultiScale## i zwrócić z endpointu liczbę wykrytych osób.
+
+        url = request.args.get('url')
+        try:
+            with urllib.request.urlopen(url) as web_file, open('image.jpg', 'wb') as local_file:
+                local_file.write(web_file.read())
+        except urllib.error.URLError as e:
+            print(e)
+        print('url', url)
+        image = cv2.imread('image.jpg')
+        image = cv2.resize(image, (700, 400))
+            # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        return {'peopleCount': len(rects)}
+
+
 api.add_resource(PeopleCounter, '/')
+api.add_resource(PeopleCounterDynamicUrl, '/dynamic')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
